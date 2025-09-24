9e775d1fc21d390479afb8037244a0c98861c59b
tabelhal <152325332+tabelhal@users.noreply.github.com>
2025-09-24 20:02:12 +0100

    Aula 1

---
+++ todolist/todolistapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks= Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'todolist/index.html', context)

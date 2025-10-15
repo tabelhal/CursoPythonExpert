c9f5c35fa9c46266c6e88e91abb5b409c1fe504e
tabelhal <152325332+tabelhal@users.noreply.github.com>
2025-10-15 20:01:21 +0100

    Aula 4

---
+++ blog/blogapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create')
+]

cad243776038fe768c440f924d73ac80bfccc867
tabelhal <152325332+tabelhal@users.noreply.github.com>
2026-01-28 17:42:53 +0000

    28 de Janeiro

--- PyQt5/PyQt_teste.py
+++ PyQt5/PyQt_teste.py
@@ -3,10 +3,6 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel, QScrollArea, QFrame
 )
 
-def greeting():
-    value = text_entry.text()
-    label.setText('Olá '+value+'! :D')
-
 
 app = QApplication(sys.argv)
 window = QWidget()
@@ -14,12 +10,6 @@ window.setWindowTitle('Hello World!')
 window.setGeometry(500, 300, 600, 600)
 
 layout = QVBoxLayout()
-text_entry = QLineEdit()
-button = QPushButton("Dizer olá!")
-label = QLabel('')
-layout.addWidget(text_entry)
-layout.addWidget(button)
-layout.addWidget(label)
 scroll = QScrollArea()
 scroll.setWidgetResizable(True)
 content = QWidget()
@@ -34,15 +24,12 @@ for i in range(10):
     box_layout.addWidget(QLabel(f"Title #{i+1}"))
     box_layout.addWidget(QLabel("From {Source}, {Date}"))
     box_layout.addWidget(QLabel("{Description}"))
-    box_layout.addWidget(QPushButton("Action"))
+    box_layout.addWidget(QPushButton("Button"))
 
     content_layout.addWidget(box)
 
 scroll.setWidget(content)
 layout.addWidget(scroll)
-
-
-button.clicked.connect(greeting)
 
 window.setLayout(layout)
 window.show()

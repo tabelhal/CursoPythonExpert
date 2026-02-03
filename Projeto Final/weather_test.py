d84d87247224c853d38ee7e13fa1d5b5ebc5ee8d
tabelhal <152325332+tabelhal@users.noreply.github.com>
2026-02-03 16:17:20 +0000

    3 de fevereiro
    Tentar as api's keys no weather_test
    Implementar a metrologia
    Ver se dá para por relógio
    Testar tudo

--- Projeto Final/weather_test.py
+++ Projeto Final/weather_test.py
@@ -1,12 +1,31 @@
-# pip install python-weather
-import python_weather
-import asyncio
+import requests
+import geocoder
 
-async def get_weather():
-    async with python_weather.Client(unit=python_weather.METRIC) as client:
-        weather = await client.get('Lisbon')
-        print(f"Temperatura atual: {weather.temperature}°C")
-        print(f"Condição: {weather.description}")
+#b7256228f73ed0b4b063df5a01f3efa7
+#86b49b06bf5482d98a9f540754b726c5
 
-if __name__ == '__main__':
-    asyncio.run(get_weather())
+API_KEY = ""
+
+g = geocoder.ip('me')
+
+lat, lon = g.latlng
+
+url = (
+    f"https://api.openweathermap.org/data/2.5/weather?"
+    f"lat={lat}&lon={lon}&appid={API_KEY}&units=metric&lang=pt_br"
+)
+
+response = requests.get(url)
+dados = response.json()
+
+if response.status_code == 200:
+    cidade = dados["name"]
+    temperatura = dados["main"]["temp"]
+    condicao = dados["weather"][0]["description"]
+
+    print(f"📍 Cidade: {cidade}")
+    print(f"🌡️ Temperatura: {temperatura}°C")
+    print(f"☁️ Condição do tempo: {condicao}")
+else:
+    print("❌ Erro ao obter o clima:")
+    print(dados)
